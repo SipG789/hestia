c229104bb0f948a0e32ffdad3d31559223dc11e7
M Munter <mark.munter1@gmail.com>
2022-03-17 14:43:51 -0500

    cleaned up code from first iteration

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,4 @@
 
-
-// var apiCall = function(){
-
-
-
-// fetch("www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata", {
-//   "method": "GET",
-//   "headers": {
-//     "x-rapidapi-host": "wyre-data.p.rapidapi.com",
-//     "x-rapidapi-key": "8a2b8cfca4msh25430a8b7cfaa1fp16b84fjsn7e759bdbf8a9"
-//   }
-//   })
-//   .then(function (response){
-//     if(response.ok){
-//       response.json().then(function(data){
-//         console.log(data);
-//       })
-//     }
-//   })
-// .catch(err => {
-//   console.error(err);
-//   console.log(data)
-// });
-// }
-// var verb = document.getElementById("verb").value;
 var food = document.getElementById("fname").value;
 
 var apiCall = function(food){
@@ -38,5 +13,5 @@ var apiCall = function(food){
 
 console.log(food)
 
-// document.getElementById("submit").addEventListener("click", testVerbs);
+
 document.getElementById('Submit').addEventListener('click', apiCall(food));
