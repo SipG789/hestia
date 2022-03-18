d00d86f35fb74f000cb480af702e350b1baff599
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-03-17 22:43:21 -0500

    added another fetch inside the main fetch to the some data that the main fetch dont have, and added events to every p tag that has a city name

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,6 +13,43 @@ var cityText = document.querySelectorAll('.city').textContent;
 // all the buttons that will listen for event
 var searchBtn = document.getElementById('search'); //1 search button by input value
 
+var austinBtn = document.querySelector('.austin'); //2 austin p tag class name
+console.log(`austin is: ${austinBtn}`);
+
+var chicagoBtn = document.querySelector('.chicago'); //3 chicago p tag class name
+console.log(`chicago is: ${chicagoBtn}`);
+
+var orlandoBtn = document.querySelector('.orlando'); //4 orlando p tag class name
+console.log(`orlando is: ${orlandoBtn}`);
+
+var sanFranciscoBtn = document.querySelector('.san-francisco');//5 san francisco p tag class name
+console.log(`san francisco is: ${sanFranciscoBtn}`);
+
+var seattleBtn = document.querySelector('.seattle');//6 seattle p tag class name
+console.log(`seattle is: ${seattleBtn}`);
+
+var newYorkBtn = document.querySelector('.new-york');//7 new york p tag class name
+console.log(`new york is: ${newYorkBtn}`);
+
+var denverBtn = document.querySelector('.denver');//8 denver p tag class name
+console.log(`denver is: ${denverBtn}`);
+
+var atlantaBtn = document.querySelector('.atlanta');//9  atlanta p tag class name
+console.log(`atlanta is: ${atlantaBtn}`);
+
+
+// all the variables that the event listeners will get the values from 
+var austin = document.getElementById('city-austin-value'); 
+var chicago = document.getElementById('city-chicago-value');
+var newYork = document.getElementById('city-new-york-value');
+var orlando = document.getElementById('city-orlando-value');
+var sanFrancisco = document.getElementById('city-san-francisco-value');
+var seattle = document.getElementById('city-seattle-value');
+var denver = document.getElementById('city-denver-value');
+var atlanta = document.getElementById('city-atlanta-value');
+var sanAntonio = document.getElementById('city-san-antonio-value');
+console.log(`new york is:${newYork}`);
+
 
 function getApi (city){
   return function(e) {
@@ -68,3 +105,13 @@ function getApi (city){
 }
 
 searchBtn.addEventListener('click', getApi(userInput));
+austinBtn.addEventListener('click', getApi(austin));
+chicagoBtn.addEventListener('click', getApi(chicago));
+newYorkBtn.addEventListener('click', getApi(newYork));
+orlandoBtn.addEventListener('click', getApi(orlando));
+sanFranciscoBtn.addEventListener('click', getApi(sanFrancisco));
+seattleBtn.addEventListener('click', getApi(seattle));
+denverBtn.addEventListener('click', getApi(denver));
+atlantaBtn.addEventListener('click', getApi(atlanta));
+window.addEventListener("DOMContentLoaded", getApi(sanAntonio));
+
