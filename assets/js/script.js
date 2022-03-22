decefca5ca5f07ef86aaf24ffacc3b02759eb1f5
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-03-22 13:41:21 -0500

    recepies show

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,18 +2,17 @@ var menu = document.getElementById('menu');
 var modal = document.querySelector('.modal-overlay');
 
 function getInputValue() {
-    // show the modal
-    modal.style.visibility = 'visible'; 
-
   // Selecting the input element and get its value 
   let food = document.getElementById("inputId").value;
   // Displaying the value
   apiCall(food)
 }
 
-var apiCall = function(food) {
 
-//to prevent CORS errors.
+var apiCall = function(food) {
+// show the modal
+modal.style.visibility = 'visible'; 
+
 //var requestUrl1 = "https://cors-anywhere.herokuapp.com/" + "https://www.themealdb.com/api/json/v1/1/search.php?s=" + food;
 var requestUrl1 = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + food;
 
@@ -26,14 +25,16 @@ if( data.meals != null){
   console.log("yes")
 
 console.log(typeof(data.meals))
-
-
+console.log(data);
+// if(data.meals)
 var meal = data.meals[0]
 
-
+// console.log(test.length)
 
 var listGroupE1 = document.createElement("ul");//ul 
+listGroupE1.classList = 'ul'
 var listHeader = document.createElement('lh')
+listHeader.classList = 'header';
 var listInstructions = document.createElement('lh')
 listGroupE1.appendChild(listHeader)
 listGroupE1.appendChild(listInstructions)
@@ -51,14 +52,15 @@ for (var i = 1; i <= 20; i++){
   }
   
  
- 
+  // console.log(meal[ingredient].length)
 
   listHeader.textContent = (meal['strMeal'])
   listInstructions.textContent = (meal['strInstructions'])
 
 
-  menu.appendChild(listHeader);
-  menu.appendChild(listGroupE1);//html + ul (li's)
+}
+menu.appendChild(listHeader);
+menu.appendChild(listGroupE1);//html + ul (li's)
 }
 else{
  var error = document.createElement('h1')
@@ -68,20 +70,15 @@ menu.appendChild(error)
 })
 
 
-
 }
 
 function startAgain(){
   modal.style.visibility = 'hidden';
-  document.location.reload();
+  document.location.reload(true);
 }
-
 
 document.getElementById('button-search').addEventListener('click', getInputValue);
 document.querySelector('.btn-close').addEventListener('click', startAgain)
-
-
-
 
 
 
